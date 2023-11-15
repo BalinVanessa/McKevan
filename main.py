18e35f9caece80cf6cbb8285cb1014f4b8a1ae01
McKenna Q <quam.m@northeastern.edu>
2023-11-15 16:02:43 -0500

    Added Screens for Adding New Goal
    Added rudimentary screens for adding a new goal and the transitions between them, as well added comments to kv file

--- main.py
+++ main.py
@@ -6,24 +6,45 @@ from kivy.core.window import Window
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.anchorlayout import AnchorLayout
 
+#Screens -
+#home screen
 class HomeScreen(Screen):
     pass
 
-class ActiveGoalScreen(Screen):
-    pass
-
-class NewGoalScreen(Screen):
-    pass
+#my fish screen
 class MyFish(Screen):
     pass
 
+#active goal screen
+class ActiveGoalScreen(Screen):
+    pass
+
+#new goal page
+class NewGoalScreen(Screen):
+    pass
+
+#new goal form
+#name of goal
+class NewGoal_Name(Screen):
+    pass
+#times to achive goal
+class NewGoal_Times(Screen):
+    pass
+#reminders of goal
+class NewGoal_Reminders(Screen):
+    pass
+
+#Screen Manager -
 class WindowManager(ScreenManager):
     pass
 
+#Line which builds the kv file
 kv = Builder.load_file("my.kv")
 
+#main function
 class MyApp(App):
     def build(self):
+        #makes the window white
         Window.clearcolor = (1, 1, 1, 1)
         return kv
 if __name__ == '__main__':
